b0172d7d40e8acf5092768b2936dedf252434ed7
carello <cp2carello@gmail.com>
2018-09-26 19:50:02 -0500

    added bin dir

---
+++ bin/env2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# run this script from the command: "source gadd_setup_basic.sh"
+
+
+# Static environmental variables.
+export TOKEN_INPUT="Bearer sometoken2YtMTU2ZS00MzE0LThhOGQtNzM2YjNiMjI5Z...."
+export APIC_URL_INPUT='http://x.x.x.x'
+export APIC_LOGIN_INPUT='someuser'
+export APIC_PASSWRD_INPUT='somepassword'
+export NFVIS_LOGIN_INPUT='someuser'
+export NFVIS_PASSWRD_INPUT='somepassword'
+export NFVIS_URL_INPUT='https://x.x.x.x'
+echo "DONE"
